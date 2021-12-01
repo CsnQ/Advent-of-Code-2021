c4b697ca7e5dffe88e7bf510ea166db9d6fc849a
CsnQ <c.quilley@gmail.com>
2021-12-01 17:46:57 +0000

    minor edits

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ from src.utility.read_from_file import read_list_from_file_as_int
 
 
 def run_puzzles():
-    input = read_list_from_file_as_int("input/day1_part1_real_input.txt")
+    input = read_list_from_file_as_int("input/day_1_real_input.txt")
     get_part1_answer(input)
     get_part2_answer(input)
+
 
 if __name__ == '__main__':
     run_puzzles()
