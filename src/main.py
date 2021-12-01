45213c002d0f3ec9d70ded6509f633677cfbe2c7
CsnQ <c.quilley@gmail.com>
2021-12-01 08:52:03 +0000

    initial setup

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from src.utility.read_from_file import read_list_from_file
+
+
+def run_puzzles():
+    read_list_from_file("input/day1_part1_test_input.txt")
+
+
+if __name__ == '__main__':
+    run_puzzles()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
